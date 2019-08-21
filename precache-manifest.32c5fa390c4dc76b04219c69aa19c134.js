3e127d2d0b5a73e95266cecf125facac4c375371
xingxun <xingxun@walkerljl.com>
2019-08-21 17:43:53 +0800

    Updates

---
+++ precache-manifest.32c5fa390c4dc76b04219c69aa19c134.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "189f6f4311c7143e5596843d4f3326e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "652a6497501e2c830b30",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "32f7d4656231ffff777b",
+    "url": "/static/js/2.bce025a2.chunk.js"
+  },
+  {
+    "revision": "652a6497501e2c830b30",
+    "url": "/static/js/main.26b1d31b.chunk.js"
+  },
+  {
+    "revision": "1abb050e395f04454103",
+    "url": "/static/js/runtime~main.75ae4e37.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
